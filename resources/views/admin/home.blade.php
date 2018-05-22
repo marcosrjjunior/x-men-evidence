9ba46abd977e502e8d88862309472cbdfa7058ea
marcosrjjunior <marcosrjjunior@gmail.com>
2018-05-22 07:13:31 -0300

    Admin tweaks

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -16,16 +16,6 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            <div class="card mb-3">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    : )
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-10">
             <div class="card">
                 <div class="card-header">Submissions</div>
 
